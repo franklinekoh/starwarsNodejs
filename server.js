fc78f000b1d5d74089f551724bfbbc0c81e141db
fekoh <mbj/4592>
2019-07-26 19:02:04 +0100

    modified server.js and .env

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const loadApp = require('./app');
  function startServer() {
     const app = loadApp.app;
 
-    let port = process.env.DOCKER_PORT || process.env.APP_PORT || 7000;
+    let port = process.env.PORT || 5000;
     app.listen(port, ()=>{
         console.log(`Server listening on port: ${port}`);
     });
 }
 
 startServer();
- 
